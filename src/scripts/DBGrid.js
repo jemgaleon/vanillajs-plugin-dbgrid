b3ba1180c715970b8bf03f4d126ae94db1c326fd
Jem <jemgaleon@gmail.com>
2020-01-16 19:05:12 +0800

    Implement initial toggle feature

--- src/scripts/DBGrid.js
+++ src/scripts/DBGrid.js
@@ -1,10 +1,11 @@
 (function() {
     // Constructor
-    window.DBGrid = function() {
+    this.DBGrid = function() {
         const grid = this;
         // Define options defaults
         const defaults = {
             gridName: "",
+            additionalCriteria: "",
             allowSorting: true,
             allowPaging: true,
             pageSize: 10,
@@ -18,12 +19,15 @@
               rowClick: null,
               rowCreated: null,
               checkAll: null,
-              check: null
+              check: null,
+              toggleAll: null,
+              toggle: null
             },
             dataKeyNames: [],
             customFields: [
               // Support pre-defined controls first
-              { type: "", includeInHeader: false, index: 0 }
+              { type: "toggle", hideColumn: true, autoOpen: true },
+              { type: "checkbox" }
             ]
         }
 
@@ -61,7 +65,9 @@
         grid.createTableBody();
       },
       getColumns: function() {
-        // todo web api call
+        // todo web api call, based on gridname
+        const grid = this;
+        
         return [
           { text: "Exam Key", value: "EXAM_KEY", type: "number", hidden: true, dataKey: true },
           { text: "Case Key", value: "CASE_KEY", type: "number", hidden: true, dataKey: true },
@@ -76,6 +82,8 @@
         ]
       },
       getRowData: function () {
+        const grid = this;
+
         // todo web api call
         return [
           [1,1,"FA","Normal","2009-0003","","","Ready For Review","2009-000-1234",""],
@@ -87,7 +95,7 @@
           [7,1,"CSAS","Normal","2009-0003","01/01/2020","","Approved","2009-000-1234","3,4,5"]
         ];
       },
-      createTable: function() {        
+      createTable: function() {
           const grid = this;
           const table = document.createElement("table");
 
@@ -146,57 +154,58 @@
         const grid = this;
         
       },
-      createTableRow: function(props) {
+      createTableRow: function() {
         const grid = this;
+        const props = arguments[0];
         const tr = document.createElement("tr");
 
         // Create custom row data
         if (grid.options.customFields.length > 0) {
-          let customFields = grid.options.customFields;
+          const customFields = grid.options.customFields;
 
           customFields.forEach(function(customField, index) {
-            let td = null;
             let properties = {};
             
             // Checkbox
             if (customField.type === "checkbox") {
-              if ((props.isColumn && customField.includeInHeader)
-                || !props.isColumn) {
-                properties = {
-                  cellType: "input|type=checkbox",
-                  isColumn: props.isColumn
-                };
-              }
-              else {
-                properties = {
-                  cellValue: "",
-                  isColumn: props.isColumn
-                };
-              }
-
-              td = grid.createTableData(properties);
-              tr.appendChild(td);
+              properties = {
+                cellType: "input|type=checkbox",
+                isColumn: props.isColumn
+              };
             }
+            // Toggle
+            else if (customField.type === "toggle") {
+              properties = {
+                cellType: "img|type=toggle,alt=collapse",
+                isColumn: props.isColumn,
+                autoOpen: customField.autoOpen,
+                hideColumn: customField.hideColumn
+              };
+            }
+
+            const td = grid.createTableData.call(grid, properties, tr);
+            tr.appendChild(td);
           });
         }
       
         // Create row data
         for (let i = 0; i < props.rowData.length; i++) {
-          const td = grid.createTableData({
+          const properties = {
             cellIndex: i + Number(grid.options.customFields.length),
             cellValue: props.rowData[i].cellValue,
             cellType: props.rowData[i].type,
             hidden: props.rowData[i].hidden,
             isColumn: props.isColumn,
             sortName: props.isColumn ? props.rowData[i].sortName : ""
-          });
+          };
+          const td = grid.createTableData.call(grid, properties, tr);
 
           // Add data Key
           if (!props.isColumn
             && props.rowData[i].dataKeyName) {
             const dataKeyName = props.rowData[i].dataKeyName;
 
-            tr.dataset[dataKeyName] = props.rowData[i].cellValue;
+            tr.dataset[dataKeyName.toLowerCase()] = props.rowData[i].cellValue;
           }
       
           tr.appendChild(td);
@@ -213,16 +222,17 @@
 
         return tr;
       },
-      createTableData: function(props) {   
+      createTableData: function() {
         const grid = this;
+        const props = arguments[0];
+        const tr = arguments[1];
 
         const td = props.isColumn 
           ? document.createElement("th") 
           : document.createElement("td");
         const span = document.createElement("span");
-        let cellValue = null;
       
-        // Class
+        // Add class
         if (props.hidden) {
           td.classList.add("hidden");
         }
@@ -230,32 +240,18 @@
         // Custom field
         if (props.cellType
           && props.cellType.indexOf("|") > -1) {
-          const control = grid.createCustomControl.call(grid, td, props);
-
+          const control = grid.createCustomControl.call(grid, props, tr, td);
+            
           span.appendChild(control);
-        } else {
-          // Set value
-          if (props.isColumn) {
-            cellValue = props.cellValue;
-          } else {
-            // Convert to data type
-            if (props.cellType === "date") {
-              cellValue =
-                props.cellValue !== ""
-                  ? new Date(props.cellValue).toLocaleDateString()
-                  : props.cellValue;
-            } else if (props.cellType === "number") {
-              cellValue = Number(props.cellValue);
-            } else {
-              cellValue = props.cellValue;
-            }
-          }
-
+        }
+        // Text only field
+        else {
           // Sorting
-          if (props.isColumn
+          if (grid.options.allowSorting
+            && props.isColumn
             && !props.hidden
-            && grid.options.allowSorting) {
-              // Class
+            && props.cellValue !== "") {
+              // Add class
               span.classList.add("sortable");
               
               // Attributes
@@ -265,16 +261,25 @@
               // Event
               span.addEventListener("click", grid.on.sort.bind(grid, span));
           }
-      
-          span.appendChild(document.createTextNode(cellValue));
+          
+          span.appendChild(document.createTextNode(
+            grid.utility.parseValue(
+              props.cellValue, 
+              props.isColumn 
+                ? ""
+                : props.cellType)));
         }
         
         td.appendChild(span);
       
         return td;
       },
-      createCustomControl: function(td, props) {
+      createCustomControl: function() {
         const grid = this;
+        const props = arguments[0];
+        const tr = arguments[1];
+        const td = arguments[2];
+        
         const element = props.cellType.split("|")[0];
         const attributes =  String(props.cellType.split("|")[1]);
         const control = document.createElement(element);
@@ -294,18 +299,45 @@
           
         // Checkbox
         if (elementType === "checkbox") {
-          // Class
-          td.classList.add("checkbox");
+          // Add class
+          td.classList.add("icon-size");
     
           // Events
           if (props.isColumn) {              
             control.addEventListener("click", grid.on.checkAll.bind(grid, control));
           }
           else {
-            control.addEventListener("click", grid.on.check.bind(grid, control));
+            control.addEventListener("click", grid.on.check.bind(grid, tr, control));
           }
         }
-        
+        // Toggle
+        else if (elementType === "toggle") {
+          // Add class
+          td.classList.add("icon-size");
+
+          if (props.isColumn 
+            && props.hideColumn) {
+            control.classList.add("hidden");
+          }
+          
+          if (props.autoOpen) {
+            control.toggled = false;
+            control.src = "/src/assets/toggle_collapse.png";
+          }
+          else {
+            control.toggled = true;
+            control.src = "/src/assets/toggle_expand.png";
+          }
+
+          // Events
+          if (props.isColumn) {
+            control.addEventListener("click", grid.on.toggleAll.bind(grid, control));
+          }
+          else {
+            control.addEventListener("click", grid.on.toggle.bind(grid, tr, control));
+          }
+        }
+
         return control;
       },
       on: {
@@ -339,7 +371,7 @@
           
           if (grid.events.rowCreated
             && typeof grid.events.rowCreated === "function") {
-              grid.events.rowCreated(sender, event);
+              grid.events.rowCreated.call(grid, sender, event);
           }
         },
         rowClick: function(sender, event) {
@@ -357,7 +389,7 @@
           if (!grid.options.cancelSelectOnClick
             && grid.events.rowClick 
             && typeof grid.events.rowClick === "function") {
-            grid.events.rowClick(sender, event);
+            grid.events.rowClick.call(grid, sender, event);
           }
         },
         checkAll: function(sender, event) {
@@ -374,10 +406,10 @@
           // Call user-defined check all event
           if (grid.events.checkAll
             && typeof grid.events.checkAll === "function") {
-            grid.events.checkAll(sender, event);
+            grid.events.checkAll.call(grid, sender, event);
           }
         },
-        check: function(sender, event) {
+        check: function(parent, sender, event) {
           const grid = this;
 
           // Do default behavior first
@@ -403,10 +435,77 @@
           // Call user-defined check event
           if (grid.events.check 
             && typeof grid.events.check === "function") {
-            grid.events.check(sender, event);
+            grid.events.check.call(parent, sender, event);
           }
           
           event.stopPropagation();
+        },
+        toggleAll: function(sender, event) {
+          const grid = this;
+          
+          // Do default behavior first
+          const imgToggles = grid.table.querySelectorAll("tbody img[type='toggle']");
+
+          sender.toggled = !sender.toggled;
+          
+          if (sender.toggle) {
+            sender.src = "/src/assets/toggle_expand.png";
+          }
+          else {
+            sender.src = "/src/assets/toggle_collapse.png";
+          }
+
+          imgToggles.forEach(function(imgToggle, index) {
+            imgToggles.toggled = !imgToggles.toggled;
+            imgToggle.click();
+          });
+
+          // Call user-defined check event
+          if (grid.events.toggleAll 
+            && typeof grid.events.toggleAll === "function") {
+            grid.events.toggleAll.call(grid, sender, event);
+          }
+
+          event.stopPropagation();
+        },
+        toggle: function(parent, sender, event) {
+          const grid = this;
+
+          // Do default behavior first
+          sender.toggled = !sender.toggled;
+          
+          if (sender.toggled) {
+            sender.src = "/src/assets/toggle_expand.png";
+          }
+          else {
+            sender.src = "/src/assets/toggle_collapse.png";
+          }
+
+          // Call user-defined check event
+          if (grid.events.toggle 
+            && typeof grid.events.toggle === "function") {
+            grid.events.toggle.call(parent, sender, event);
+          }
+
+          event.stopPropagation();
+        }
+      },
+      utility: {
+        parseValue: function(value, type) {
+          let newValue = null;
+
+          if (type === "date") {
+            newValue =
+              value !== ""
+                ? new Date(value).toLocaleDateString()
+                : value;
+          } else if (type === "number") {
+            newValue = Number(value);
+          } else {
+            newValue = value;
+          }
+
+          return newValue;
         }
       }
     };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,7 @@ let dbgrid;
   // Instance DBGrid Obj
   dbgrid = new DBGrid({
     gridName: "WORKLIST",
+    additionalCriteria: "",
     cancelSelectOnClick: false,
     allowSorting: true,
     dataKeyNames: ["EXAM_KEY,CASE_KEY,SECTION,PRIORITY"],
@@ -14,18 +15,27 @@ let dbgrid;
         //console.log(sender);
       },
       rowClick: function(sender, event) {
+        console.log(sender);
         console.log(sender.dataset);
-        //console.log("user row click");
+        console.log("user row click");
       },
       checkAll: function(sender, event) {
         console.log("user check all");
       },
       check: function(sender, event) {
         console.log("user check");
+        console.log(this);
+      },
+      toggle: function(sender, event) {
+        console.log("user toggle: " + sender.toggled);
+        console.log(this);
+        
+        // todo need a better way to get selected row
       }
     },
     customFields: [
-      { type: "checkbox", index: 0, includeInHeader: true }
+      { type: "toggle", hideColumn: true, autoOpen: true },
+      { type: "checkbox" }
     ]
   });
   
